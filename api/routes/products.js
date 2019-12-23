6377e323082e7a22874256a16e112406a1fb42ea
ken <kenmugy@gmail.com>
2019-12-23 20:19:43 +0300

    made few minor changes to requests

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,9 +3,10 @@ module.exports = Product => {
 
   router.get('/', async (req, res) => {
     try {
-      const products = await Product.find();
+      const products = await Product.find().select('name price _id');
       res.status(200).json({
         message: 'handling GET reqs for /products',
+        count: products.length,
         products
       });
     } catch (error) {
@@ -14,18 +15,18 @@ module.exports = Product => {
   });
 
   router.post('/', async (req, res) => {
+    const product = new Product({
+      name: req.body.name,
+      price: req.body.price
+    });
     try {
-      const product = new Product({
-        name: req.body.name,
-        price: req.body.price
-      });
       const savedProduct = await product.save();
       res.status(201).json({
         message: 'Handling POST req for /products',
         createdProduct: savedProduct
       });
     } catch (error) {
-      console.log(error);
+      res.status(500).json({ error: error.message });
     }
   });
 
@@ -37,7 +38,6 @@ module.exports = Product => {
         return next();
       }
     } catch (error) {
-      console.log(error);
       return res.status(404).json({ error: error.message });
     }
   });
@@ -46,10 +46,10 @@ module.exports = Product => {
     .route('/:productID')
     .get((req, res) => res.status(201).json(req.product))
     .put(async (req, res) => {
+      const { product } = req;
+      product.name = req.body.name;
+      product.price = req.body.price;
       try {
-        const { product } = req;
-        product.name = req.body.name;
-        product.price = req.body.price;
         const savedProduct = await product.save();
         res.status(200).json({
           message: 'REPLACED product',
@@ -60,14 +60,14 @@ module.exports = Product => {
       }
     })
     .patch(async (req, res) => {
+      const { product } = req;
+      req.body._id && delete req.body._id;
+      Object.entries(req.body).forEach(entry => {
+        const key = entry[0];
+        const value = entry[1];
+        product[key] = value;
+      });
       try {
-        const { product } = req;
-        req.body._id && delete req.body._id;
-        Object.entries(req.body).forEach(entry => {
-          const key = entry[0];
-          const value = entry[1];
-          product[key] = value;
-        });
         const patchedProduct = await product.save();
         res.status(200).json({
           message: 'PATCHED product',
@@ -80,9 +80,9 @@ module.exports = Product => {
     .delete(async (req, res) => {
       try {
         await req.product.remove();
-        res.sendStatus(204)
+        res.sendStatus(204);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     });
 
